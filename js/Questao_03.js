3f84c274cbd4ee7d73a6a1eb0b17123a139fe586
Kaique Viana <110206582+vian4Kaique@users.noreply.github.com>
2022-09-23 01:05:58 +0000

    versão 1

---
+++ js/Questao_03.js
@@ -0,0 +1,8 @@
+var nome = "João da Silva";
+var n1 = 7;
+var n2 = 8.5;
+var n3 = n1 * 0.6;
+var n4 = n2 * 0.4;
+var n5 = n3 + n4;
+
+alert(nome + n1 + n2);
---
+++ js/Questao_04.js
@@ -0,0 +1,21 @@
+class Aluno{
+	constructor(primeiroNome, segundoNome, primeiraNota, segundaNota){
+			tris.primeiroNome = primeiroNome;
+			tris.segundoNome = segundoNome;
+			tris.primeiraNota = primeiraNota;
+			tris.segundaNota = segundaNota;
+	}
+	nomeCompleto(){
+		alert(tris.primeiroNome + tris.segundoNome);
+	}
+	media(){
+		
+	}	
+	
+	situacao(){
+		if()	
+	}
+
+	
+	
+}
